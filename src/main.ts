6840ed2e15df4c0da4422466bbfd68a6e548c6fa
zchengfa <1522977814@qq.com>
2024-03-25 16:39:11 +0800

    refactor(安装自定义插件):

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import {createApp} from "vue";
 import App from "./App.vue";
 import {createPinia} from "pinia";
 import ElementPlus from 'element-plus/lib/index'
+import toast from "../Components/Toast/toast";
+import loading from "../Components/Loading/loading";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(ElementPlus)
+
+//安装自定义插件
+app.use(toast)
+app.use(loading)
 
 app.mount('#app')
